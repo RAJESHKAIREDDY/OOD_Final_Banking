53793eaea64ce2273c101fe1420ff6e7a15a1ab6
Sri Harsha Peri <peri.s@northeastern.edu>
2023-04-17 01:04:00 -0400

    fixed Label not display error in AcctTransactionsAPController

--- SafeBankFX/src/application/TransactionsSceneController.java
+++ SafeBankFX/src/application/TransactionsSceneController.java
@@ -88,7 +88,6 @@ public class TransactionsSceneController extends Controller implements Initializ
 				radioBtnCredit.setVisible(false);
 				radioBtnSavings.setVisible(false);
 				anchorPane.getChildren().setAll(accounts.getChildren());
-				
 			}
 			else {
 				if(radioBtnCredit.isSelected()) {
